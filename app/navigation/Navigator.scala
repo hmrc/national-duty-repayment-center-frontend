a1b060139647b8ddce5439a84495d3e83a5a41ce
somaraja <8107928+somaraja@users.noreply.github.com>
2020-11-12 21:30:27 +0000

    NDRCN-142 Navigation Changes

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -17,27 +17,29 @@
 package navigation
 
 import javax.inject.{Inject, Singleton}
-
 import play.api.mvc.Call
 import controllers.routes
 import pages._
 import models._
+
+import scala.collection.mutable
 
 @Singleton
 class Navigator @Inject()() {
 
   private val normalRoutes: Page => UserAnswers => Call = {
     case ClaimantTypePage => _ => routes.NumberOfEntriesTypeController.onPageLoad(NormalMode)
-    case NumberOfEntriesTypePage => _ => routes.CustomsRegulationTypeController.onPageLoad(NormalMode)
+    case NumberOfEntriesTypePage  => howManyEntries
+    case HowManyEntriesPage  => _ => routes.CustomsRegulationTypeController.onPageLoad(NormalMode)
     case CustomsRegulationTypePage => _ => routes.ArticleTypeController.onPageLoad(NormalMode)
     case ArticleTypePage => _ => routes.ClaimEpuController.onPageLoad(NormalMode)
     case ClaimEpuPage => _ => routes.ClaimEntryNumberController.onPageLoad(NormalMode)
     case ClaimEntryNumberPage => _ => routes.ClaimEntryDateController.onPageLoad(NormalMode)
     case ClaimEntryDatePage => _ => routes.ClaimReasonTypeController.onPageLoad(NormalMode)
     case ClaimReasonTypePage => _ => routes.ReasonForOverpaymentController.onPageLoad(NormalMode)
-    case ReasonForOverpaymentPage => _ => routes.WhatAreTheGoodsController.onPageLoad(NormalMode)
+    case ReasonForOverpaymentPage => _ => routes. WhatAreTheGoodsController.onPageLoad(NormalMode)
     case WhatAreTheGoodsPage => _ => routes.ClaimRepaymentTypeController.onPageLoad(NormalMode)
-    case ClaimRepaymentTypePage => _ => routes.CustomsDutyPaidController.onPageLoad(NormalMode)
+    case ClaimRepaymentTypePage => claimRepaymentType
     case CustomsDutyPaidPage => _ => routes.CustomsDutyDueToHMRCController.onPageLoad(NormalMode)
     case CustomsDutyDueToHMRCPage => _ => routes.VATPaidController.onPageLoad(NormalMode)
     case VATPaidPage => _ => routes.VATDueToHMRCController.onPageLoad(NormalMode)
@@ -45,8 +47,21 @@ class Navigator @Inject()() {
     case OtherDutiesPaidPage => _ => routes.OtherDutiesDueToHMRCController.onPageLoad(NormalMode)
     case OtherDutiesDueToHMRCPage => _ => routes.RepaymentAmountSummaryController.onPageLoad
 
-
   }
+
+  private def howManyEntries(answers: UserAnswers): Call = answers.get(NumberOfEntriesTypePage) match {
+    case Some(NumberOfEntriesType.Single)  => routes.CustomsRegulationTypeController.onPageLoad(NormalMode)
+    case Some(NumberOfEntriesType.Multiple) => routes.HowManyEntriesController.onPageLoad(NormalMode)
+    case None => routes.SessionExpiredController.onPageLoad()
+  }
+
+  private def claimRepaymentType(answers: UserAnswers): Set[Call] = answers.get(ClaimRepaymentTypePage).get.map
+  { result =>
+      result match {
+    case ClaimRepaymentType.Customs  => routes.CustomsDutyPaidController.onPageLoad(NormalMode)
+    case ClaimRepaymentType.Vat  => routes.VATPaidController.onPageLoad(NormalMode)
+    case ClaimRepaymentType.Other  => routes.OtherDutiesPaidController.onPageLoad(NormalMode)
+  } }
 
   private val checkRouteMap: Page => UserAnswers => Call = {
     case _ => _ => routes.CheckYourAnswersController.onPageLoad()
