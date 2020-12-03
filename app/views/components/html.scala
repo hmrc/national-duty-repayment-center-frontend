c82a498b71bc7087bdcfad32fed9add194aff6bd
Marcin Cichon <slashrealm@gmail.com>
2020-12-03 17:11:13 +0000

    [NDRCN-240] Create a new layout with correct styles

--- app/views/components/html.scala
+++ app/views/components/html.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,19 +12,21 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@this(
-    main_template: MainTemplate
+package views.components
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class html @Inject() (
+//  val h1: views.html.components.h1,
+//  val h2: views.html.components.h2,
+//  val h3: views.html.components.h3,
+//  val p: views.html.components.p,
+//  val strong: views.html.components.strong,
+//  val a: views.html.components.link,
+//  val ul: views.html.components.bullets,
+//  val ol: views.html.components.orderedList,
+  val button: views.html.components.button
 )
-
-@()(implicit request: Request[_], messages: Messages)
-
-@main_template(
-    title = messages("index.title")
-    ) {
-
-    @components.legacy.heading("index.heading")
-
-    <p>@messages("index.guidance")</p>
-}
