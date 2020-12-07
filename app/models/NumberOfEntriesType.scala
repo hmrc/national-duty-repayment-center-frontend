cc35109c20952f303355439e73b33fa1ebeb1635
shiromi <shiromi002@yahoo.co.uk>
2020-12-07 17:23:24 +0000

    NDRCN-240 customer regulation and number of entries view implemented.

--- app/models/NumberOfEntriesType.scala
+++ app/models/NumberOfEntriesType.scala
@@ -16,8 +16,10 @@
 
 package models
 
-import play.api.libs.json._
-import viewmodels.RadioOption
+import play.api.data.Form
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
 
 sealed trait NumberOfEntriesType
 
@@ -30,9 +32,13 @@ object NumberOfEntriesType extends Enumerable.Implicits {
     Single, Multiple
   )
 
-  val options: Seq[RadioOption] = values.map {
+  def options(form: Form[_])(implicit messages: Messages): Seq[RadioItem] = values.map {
     value =>
-      RadioOption("numberOfEntriesType", value.toString)
+      RadioItem(
+        value = Some(value.toString),
+        content = Text(messages(s"numberOfEntriesType.${value.toString}")),
+        checked = form("value").value.contains(value.toString)
+      )
   }
 
   implicit val enumerable: Enumerable[NumberOfEntriesType] =
