309cff328fc4be29328e0b9ad22c8e2f99378c33
somaraja <8107928+somaraja@users.noreply.github.com>
2020-12-17 15:46:22 +0000

    NDRCN-215-216

--- app/controllers/VATPaidController.scala
+++ app/controllers/VATPaidController.scala
@@ -52,7 +52,7 @@ class VATPaidController @Inject()(
         case Some(value) => form.fill(value)
       }
 
-      Ok(view(preparedForm, mode))
+      Ok(view(preparedForm, mode, request.userAnswers))
   }
 
   def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
@@ -60,7 +60,7 @@ class VATPaidController @Inject()(
 
       form.bindFromRequest().fold(
         formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+          Future.successful(BadRequest(view(formWithErrors, mode, request.userAnswers))),
 
         value =>
           for {
