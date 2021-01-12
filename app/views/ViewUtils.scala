98e1c75896cfd262dc5e7718a27486dac4a17a1c
shiromi <shiromi002@yahoo.co.uk>
2021-01-12 11:10:12 +0000

    NDRCN-246 complete unauthorised page according to new govuk frontend framwork and fixed NDRCN-327 issue 1 and 2

--- app/views/ViewUtils.scala
+++ app/views/ViewUtils.scala
@@ -18,8 +18,21 @@ package views
 
 import play.api.data.Form
 import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
+import uk.gov.hmrc.govukfrontend.views.viewmodels.hint.Hint
 
 object ViewUtils {
+
+  def renderEmpref(empref: String): String = empref.replace("-", "/")
+
+  def title(form: Form[_], titleStr: String, section: Option[String] = None, titleMessageArgs: Seq[String] = Seq())
+           (implicit messages: Messages): String = {
+    titleNoForm(s"${errorPrefix(form)} ${messages(titleStr, titleMessageArgs:_*)}", section)
+  }
+
+
+  def titleNoForm(title: String, section: Option[String] = None, titleMessageArgs: Seq[String] = Seq())(implicit messages: Messages): String =
+    s"${messages(title, titleMessageArgs:_*)} - ${section.fold("")(messages(_) + " - ")}${messages("site.service_name")} - ${messages("site.govuk")}"
 
   def errorPrefix(form: Form[_])(implicit messages: Messages): String = {
     if (form.hasErrors || form.hasGlobalErrors) messages("error.browser.title.prefix") else ""
