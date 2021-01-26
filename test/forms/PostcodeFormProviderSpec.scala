aa7fc6f094accdade7ee75944b23dad2a1bb96b0
somaraja <8107928+somaraja@users.noreply.github.com>
2021-01-26 13:02:46 +0000

    NDRCN-286 Added Testcase

---
+++ test/forms/PostcodeFormProviderSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.StringFieldBehaviours
+import play.api.data.FormError
+
+class PostcodeFormProviderSpec extends StringFieldBehaviours {
+
+  val requiredKey = "postcode.error.required"
+  val lengthKey = "postcode.error.length"
+
+  val minLength = 6
+  val maxLength = 9
+
+  val form = new PostcodeFormProvider()()
+
+  ".value" must {
+
+    val fieldName = "postCode"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMinAndMaxLength(minLength,maxLength)
+    )
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+}
