8c3c0c3c66cc495f1514613b432971084611d8b8
Sweta Chandak <sweta.chandak@digital.hmrc.gov.uk>
2021-02-24 14:01:05 +0000

    Added change for File upload

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -282,7 +282,7 @@ class CheckYourAnswersHelper(userAnswers: UserAnswers)(implicit messages: Messag
     x => {
       AnswerRow(
         HtmlFormat.escape(messages("contactByEmail.checkYourAnswersLabel")),
-        HtmlFormat.escape(x.isBlank match
+        HtmlFormat.escape(false match
               { case true => "No"
                 case _ => "Yes"}),
         Some(routes.EmailAddressController.onPageLoad(CheckMode).url)
@@ -379,7 +379,7 @@ class CheckYourAnswersHelper(userAnswers: UserAnswers)(implicit messages: Messag
       HtmlFormat.escape(messages("view.upload-file.checkYourAnswersLabel")),
       HtmlFormat.escape((userAnswers.fileUploadState.get.fileUploads.files.size.toString)
       .concat(" ").concat(messages("view.upload-file.documents.added"))),
-      Some(routes.FileUploadController.showFileUploaded.url)
+      Some(routes.FileUploadController.showFileUploaded(CheckMode).url)
     )
   }
 
