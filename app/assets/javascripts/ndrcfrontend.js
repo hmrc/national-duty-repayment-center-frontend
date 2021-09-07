ee910b0bb69f48c7e915accaa080b7aefc71d4dc
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-09-07 12:39:23 +0100

    NF-837 - add role to uploadMessage; add aria-label to progress bar; update page title on file submit (JAWS)

--- app/assets/javascripts/ndrcfrontend.js
+++ app/assets/javascripts/ndrcfrontend.js
@@ -10,15 +10,21 @@ function onFileSelect() {
     document.getElementById("ndrc-fileupload-continue").disabled = true;
     document.getElementById("upload-form").classList.add("govuk-!-display-none");
     document.querySelector(".hidden-progress-row").classList.remove("govuk-!-display-none");
+
+    var loadingMessage = document.getElementById("uploadingMessage").innerText;
+
+    document.title = loadingMessage;
+    document.getElementById("uploadingMessage").focus();
+
     var errorBlock = document.querySelector(".govuk-error-summary");
     if(errorBlock){
       errorBlock.classList.add("govuk-!-display-none");
     }
-    document.getElementById("uploadingMessage").focus();
+
   }
   finally {
     // timeout before submitting is to give Safari time to render un-hidden element
-    // animations stop in Safari once form is submitted so animation start is delayed by 500 ms
-    setTimeout(function () {  document.getElementById("upload-form").submit(); }, 500);
+    // animations stop in Safari once form is submitted so animation start is delayed by 1000 ms
+    setTimeout(function () {  document.getElementById("upload-form").submit(); }, 1000);
   }
 }
