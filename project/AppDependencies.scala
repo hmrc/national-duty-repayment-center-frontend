82223fcb36558f38ed1582c1a2e62da4649d1472
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-09-14 10:35:55 +0100

    BAU - update play-ui, play-frontend-hmrc and govuk-template libraries - remove unnecessary Metrics mocking in SpecBase

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,10 +7,10 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "5.12.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.53.0",
-    "uk.gov.hmrc"       %% "govuk-template"                 % "5.69.0-play-28",
-    "uk.gov.hmrc"       %% "play-ui"                        % "9.6.0-play-28",
+    "uk.gov.hmrc"       %% "govuk-template"                 % "5.70.0-play-28",
+    "uk.gov.hmrc"       %% "play-ui"                        % "9.7.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.9.0-play-28",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "1.4.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "1.8.0-play-28",
     "com.sun.mail"      % "javax.mail"                      % "1.6.2"
   )
 
