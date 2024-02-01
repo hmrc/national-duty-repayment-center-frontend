5198f896fdea2842ee9d429d2dc40d41cc8dad49
Tapiwa Tiyemba <142234222+tapiwa-tiyemba@users.noreply.github.com>
2024-02-01 12:44:31 +0000

    play-conditional-form-mapping

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % mongoVersion,
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.0.0-play-28",
     "com.sun.mail"       % "javax.mail"                    % "1.6.2"
   )
