52cfa76434ecd90457b8b057efd205ff9aa5ff52
AbdulHQuadri <67271481+AbdulHQuadri@users.noreply.github.com>
2026-01-23 12:15:07 +0000

    updated copyright headers

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import play.api.mvc.Call
 import scala.concurrent.duration.{FiniteDuration, MILLISECONDS}
 
 /*
- * Copyright 2021 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
