8a9817b5ba5313011823636af3e24f6f815144ab
liamhenderson92 <35490828+liamhenderson92@users.noreply.github.com>
2026-03-10 11:38:28 +0000

    DDCYLS-8143: Upgrade dependencies (#589)
    * DDCYLS-8143: Upgrade dependencies
    
    * DDCYLS-8143: Bot comment upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,25 +2,28 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val playVersion      = "play-30"
   private val mongoVersion     = "2.12.0"
+
+  val mailDependencies: Seq[ModuleID] = Seq(
+    "jakarta.mail" % "jakarta.mail-api" % "2.1.5",
+    "org.eclipse.angus" % "angus-mail" % "2.0.5"
+  )
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.31.0",
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % mongoVersion,
-    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.4.0",
-    "com.sun.mail"       % "javax.mail"                                  % "1.6.2"
-  )
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.32.0",
+    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.5.0",
+  ) ++ mailDependencies
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-test-$playVersion" % mongoVersion,
     "org.scalatestplus"    %% "scalacheck-1-17"               % "3.2.18.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"         % "1.1.0"
-  ).map(_ % Test)
+  ) ++ mailDependencies.map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
 
