5a2fe48169e8ff7d6bb56a217dc499f20ea11740
bharti.verma <68339135+bharti5verma@users.noreply.github.com>
2026-03-27 13:49:33 +0000

    DDCYLS-8266 Vulnerabilities fix

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.0"
   private val playVersion      = "play-30"
   private val mongoVersion     = "2.12.0"
 
@@ -14,8 +14,9 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.32.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "13.2.0",
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.5.0",
+    "io.github.openhtmltopdf" % "openhtmltopdf-pdfbox"       % "2.0.24"
   ) ++ mailDependencies
 
   val test: Seq[ModuleID] = Seq(
