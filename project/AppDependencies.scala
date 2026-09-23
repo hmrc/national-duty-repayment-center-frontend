29e0db83992ca99542b713be6ac9c71851f31631
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2026-09-23 11:35:09 +0100

    reorganising dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,12 +15,12 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "13.13.0",
-    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.5.0"
+    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.5.0",
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % mongoVersion
   ) ++ mailDependencies
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% s"bootstrap-test-$playVersion"  % bootstrapVersion % Test,
-    "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-test-$playVersion" % mongoVersion,
     "org.scalatestplus"    %% "scalacheck-1-17"               % "3.2.18.0" % Test,
     "io.github.wolfendale" %% "scalacheck-gen-regexp"         % "1.1.0"
   ) ++ mailDependencies.map(_ % Test)
